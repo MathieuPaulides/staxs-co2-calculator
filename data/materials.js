21f5a49a99b8eaea4719fd5ac5bc5cf5d8c7e818
MathieuPaulides <paulidesmathieu@gmail.com>
2026-01-29 14:52:24 +0100

    Update materials.js

--- data/materials.js
+++ data/materials.js
@@ -1,1 +1,7 @@
+window.DEFAULT_MATERIALS = [
+  { id: "isopreen", name: "Isopreen (band)", factor: 0.8 },
+  { id: "pc", name: "PC (Lens)", factor: 6.25 },
+  { id: "pvc", name: "PVC (frame)", factor: 2.4 },
+  { id: "tpv", name: "TPV (frame)", factor: 0.8 }
+];
 
