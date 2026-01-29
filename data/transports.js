98b6e9b2d3c96bda650b6ba1a819c29cfd854ec2
MathieuPaulides <paulidesmathieu@gmail.com>
2026-01-29 14:53:02 +0100

    Add default transport options with CO2 emissions

--- data/transports.js
+++ data/transports.js
@@ -1,1 +1,8 @@
+window.DEFAULT_TRANSPORTS = [
+  { id: "air", name: "Air Freight", co2: 0.5, default: false },
+  { id: "electric", name: "Electric truck", co2: 0.05, default: false },
+  { id: "ship", name: "Ship", co2: 0.01, default: false },
+  { id: "train", name: "Train", co2: 0.03, default: false },
+  { id: "truck", name: "Truck", co2: 0.12, default: true }
+];
 
