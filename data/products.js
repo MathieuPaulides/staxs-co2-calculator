824fba156e26dec6bee1a3d84a9ea0f0c5a68acd
MathieuPaulides <paulidesmathieu@gmail.com>
2026-01-29 14:52:49 +0100

    Add default products data to products.js

--- data/products.js
+++ data/products.js
@@ -1,1 +1,21 @@
+window.DEFAULT_PRODUCTS = [
+  {
+    id: "F-0300",
+    name: "F-0300",
+    materials: [
+      { materialId: "tpv", weight: 0.0562 },
+      { materialId: "pc", weight: 0.0367 },
+      { materialId: "isopreen", weight: 0.0129 }
+    ]
+  },
+  {
+    id: "FS-0086",
+    name: "FS-0086",
+    materials: [
+      { materialId: "pvc", weight: 0.0424 },
+      { materialId: "pc", weight: 0.02036 },
+      { materialId: "isopreen", weight: 0.00463 }
+    ]
+  }
+];
 
